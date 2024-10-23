711b1cd0d2680f292a6dc941e6eea2bd084a385b
Matus Lhongpol <matuslhongpol@gmail.com>
2024-10-23 22:09:22 +0700

    Finish chapter 11

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,4 +1,6 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
+
+export const experimental_ppr = true;
  
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -10,3 +12,4 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     </div>
   );
 }
+
